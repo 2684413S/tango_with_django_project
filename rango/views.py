a0af015a2b335557876fffd1115c28dc68a9f413
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-01-20 17:11:16 +0000

    completed 3.5, mapping URLs

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("RANGO says hey there partner!")
+    return HttpResponse("Rango says hey there partner!")
 
