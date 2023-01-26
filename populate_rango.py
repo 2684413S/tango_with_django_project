c806eb5402a4fc7c4be540b7ef3e04bcecbc9e92
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-01-26 15:44:14 +0000

    chap 5 updates + end of chap exercises

--- populate_rango.py
+++ populate_rango.py
@@ -6,34 +6,31 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    python_pages = [
-        {'title': 'Official Python Tutorial', 
+    python_pages = [{'title': 'Official Python Tutorial', 
         'url': 'http://docs.python.org/3/tutorial/'},
         {'title': 'How to Think like a Computer Scientist', 
         'url': 'http://www.greenteapress.com/thinkpython/'},
         {'title': 'Learn Python in 10 Minutes', 
-        'url': 'http://www.korokithakis.net/tutorials/python/'}
-
-        {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+        'url': 'http://www.korokithakis.net/tutorials/python/'}]
     
-    other_pages = [
-        {'title':'Bottle',
+    django_pages = [{'title':'Official Django Tutorial', 
+    'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+    {'title':'Django Rocks',
+    'url':'http://www.djangorocks.com/'},
+    {'title':'How to Tango with Django',
+    'url':'http://www.tangowithdjango.com/'} ] 
+    
+    other_pages = [{'title':'Bottle',
         'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
-    
-    cats = {'Python': {'pages': python_pages},
-    'Django': {'pages': django_pages},
-    'Other Frameworks': {'pages': other_pages} }
+
+    cats = {'Python': {'pages': python_pages}, 
+    'Django': {'pages': django_pages}, 
+    'Other Frameworks': {'pages': other_pages}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
-        
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category (models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    #chap5 exercise 1
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.name
 
